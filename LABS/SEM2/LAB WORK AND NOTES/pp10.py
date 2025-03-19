43edd685071ce62a63f319e8b01ba4f198ec9583
Dev202006 <devchauhan51278@gmail.com>
2025-03-19 11:51:12 +0530

    hello

--- LABS/SEM2/LAB WORK AND NOTES/pp10.py
+++ LABS/SEM2/LAB WORK AND NOTES/pp10.py
@@ -1,8 +1,11 @@
-'''
-Write a program that defines a functino called frequency() which computes the frequency of words present in a string passed to it.
-The frequencies should be returned in sorted order out of words in the string. 
-'''
-def frequency(s): 
-    slist = s.split(" ")
-    sset = set(slist)
-    sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[1])) #learn this
+# # '''
+# # Write a program that defines a functino called frequency() which computes the frequency of words present in a string passed to it.
+# # The frequencies should be returned in sorted order out of words in the string. 
+# # '''
+# def frequency(s): 
+#     slist = s.split(" ")
+#     sset = set(slist)
+#     sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[1])) #learn this
+# #HII
+
+print("hello")
